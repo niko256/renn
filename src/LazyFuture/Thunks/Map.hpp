eea463d67a3b9babf40ebe564566bb8c0ace1267
Niko256 <kolbnickolay@yandex.ru>
2026-01-27 23:53:41 +0300

    fix compilation error...

--- src/LazyFuture/Thunks/Map.hpp
+++ src/LazyFuture/Thunks/Map.hpp
@@ -24,28 +24,20 @@ struct [[nodiscard]] Map : public role::ThunkBase<Map<Upstream, F>> {
     Upstream producer_;
     F procedure_;
 
-    Map(Upstream pr, F user);
+    Map(Upstream pr, F user)
+        : producer_(std::move(pr)),
+          procedure_(std::move(user)) {}
 
     template <Continuation<OutputType> Downstream>
-    Computation auto materialize(Downstream cont);
+    Computation auto materialize(Downstream cont) {
+        auto transformer
+            = cont::Transform<InputType, OutputType, F, Downstream>{
+                std::move(procedure_), std::move(cont)
+            };
+
+        /* let producer wrap up and materialize continuation of this stage */
+        return producer_.materialize(std::move(transformer));
+    }
 };
-
-/* |-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-| */
-
-template <Thunk Upstream, typename F>
-Map<Upstream, F>::Map(Upstream pr, F user)
-    : producer_(std::move(pr)),
-      procedure_(std::move(user)) {}
-
-template <Thunk Upstream, typename F>
-template <Continuation<typename Map<Upstream, F>::OutputType> Downstream>
-Computation auto Map<Upstream, F>::materialize(Downstream cont) {
-    auto transformer = cont::Transform<InputType, OutputType, F, Downstream>{
-        std::move(procedure_), std::move(cont)
-    };
-
-    /* let producer wrap up and materialize continuation of this stage */
-    return producer_.materialize(std::move(transformer));
-}
 
 }  // namespace renn::future::thunk
